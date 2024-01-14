22bc7ef68de400869ae008bedeac761dfbd1c720
typhonrt <support@typhonjs.io>
2024-01-14 02:13:13 -0800

    Update package source.

--- src/data/format/unicode/index.js
+++ src/data/format/unicode/index.js
@@ -1,2 +1,2 @@
-// In this case pull code from separate TRL distribution as `@typhonjs-svelte/unicode` also is independently packaged.
-export * from '../../../../node_modules/@typhonjs-svelte/unicode/dist-trl/index.js';
+// In this case pull code from separate TRL distribution as `@typhonjs-utils/unicode` also is independently packaged.
+export * from '../../../../node_modules/@typhonjs-utils/unicode/dist-trl/index.js';
