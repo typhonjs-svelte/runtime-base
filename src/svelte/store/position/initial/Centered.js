936ecdc940ea3e1536c386b36ba9c5566e3a726d
typhonrt <support@typhonjs.io>
2024-03-30 15:12:56 -0700

    Create base implementation for initial position helpers.

--- src/svelte/store/position/initial/Centered.js
+++ src/svelte/store/position/initial/Centered.js
@@ -1,154 +1,12 @@
-import { A11yHelper } from '#runtime/util/browser';
+import { InitialBase } from './InitialBase.js';
 
 /**
- * Provides a {@link TJSPositionTypes.IInitialHelper} implementation to center an element being positioned.
+ * Provides a {@link TJSPositionTypes.IInitialHelperExt} implementation to center an element being positioned.
  *
  * @implements {import('../types').TJSPositionTypes.IInitialHelperExt}
  */
-export class Centered
+export class Centered extends InitialBase
 {
-   /**
-    * @type {HTMLElement}
-    */
-   #element;
-
-   /**
-    * Provides a manual setting of the element height. As things go `offsetHeight` causes a browser layout and is not
-    * performance oriented. If manually set this height is used instead of `offsetHeight`.
-    *
-    * @type {number}
-    */
-   #height;
-
-   /**
-    * Set from an optional value in the constructor to lock accessors preventing modification.
-    */
-   #lock;
-
-   /**
-    * Provides a manual setting of the element width. As things go `offsetWidth` causes a browser layout and is not
-    * performance oriented. If manually set this width is used instead of `offsetWidth`.
-    *
-    * @type {number}
-    */
-   #width;
-
-   /**
-    * @param {object}      [options] - Initial options.
-    *
-    * @param {HTMLElement} [options.element] - Target element.
-    *
-    * @param {boolean}     [options.lock=false] - Lock parameters from being set.
-    *
-    * @param {number}      [options.width] - Manual width.
-    *
-    * @param {number}      [options.height] - Manual height.
-    */
-   constructor({ element, lock = false, width, height } = {})
-   {
-      this.element = element;
-      this.width = width;
-      this.height = height;
-
-      this.#lock = typeof lock === 'boolean' ? lock : false;
-   }
-
-   /**
-    * @returns {HTMLElement | undefined | null} Target element.
-    */
-   get element() { return this.#element; }
-
-   /**
-    * @returns {number} Get manual height.
-    */
-   get height() { return this.#height; }
-
-   /**
-    * @returns {number} Get manual width.
-    */
-   get width() { return this.#width; }
-
-   /**
-    * @param {HTMLElement | undefined | null} element - Set target element.
-    */
-   set element(element)
-   {
-      if (this.#lock) { return; }
-
-      if (element === void 0  || element === null || A11yHelper.isFocusTarget(element))
-      {
-         this.#element = element;
-      }
-      else
-      {
-         throw new TypeError(`'element' is not a HTMLElement, undefined, or null.`);
-      }
-   }
-
-   /**
-    * @param {number}   height - Set manual height.
-    */
-   set height(height)
-   {
-      if (this.#lock) { return; }
-
-      if (height === void 0 || Number.isFinite(height))
-      {
-         this.#height = height;
-      }
-      else
-      {
-         throw new TypeError(`'height' is not a finite number or undefined.`);
-      }
-   }
-
-   /**
-    * @param {number}   width - Set manual width.
-    */
-   set width(width)
-   {
-      if (this.#lock) { return; }
-
-      if (width === void 0 || Number.isFinite(width))
-      {
-         this.#width = width;
-      }
-      else
-      {
-         throw new TypeError(`'width' is not a finite number or undefined.`);
-      }
-   }
-
-   /**
-    * Set manual width & height.
-    *
-    * @param {number}   width - New manual width.
-    *
-    * @param {number}   height - New manual height.
-    */
-   setDimension(width, height)
-   {
-      if (this.#lock) { return; }
-
-      if (width === void 0 || Number.isFinite(width))
-      {
-         this.#width = width;
-      }
-      else
-      {
-         throw new TypeError(`'width' is not a finite number or undefined.`);
-      }
-
-      if (height === void 0 || Number.isFinite(height))
-      {
-         this.#height = height;
-      }
-      else
-      {
-         throw new TypeError(`'height' is not a finite number or undefined.`);
-      }
-   }
-
    /**
     * Get the left constraint based on any manual target values or the browser inner width.
     *
@@ -159,7 +17,7 @@ export class Centered
    getLeft(width)
    {
       // Determine containing bounds from manual values; or any element; lastly the browser width / height.
-      const boundsWidth = this.#width ?? this.#element?.offsetWidth ?? globalThis.innerWidth;
+      const boundsWidth = this.width ?? this.element?.offsetWidth ?? globalThis.innerWidth;
 
       return (boundsWidth - width) / 2;
    }
@@ -173,7 +31,7 @@ export class Centered
     */
    getTop(height)
    {
-      const boundsHeight = this.#height ?? this.#element?.offsetHeight ?? globalThis.innerHeight;
+      const boundsHeight = this.height ?? this.element?.offsetHeight ?? globalThis.innerHeight;
 
       return (boundsHeight - height) / 2;
    }
