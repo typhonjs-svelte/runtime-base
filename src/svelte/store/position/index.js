fccc032388d220aa1da0de81af59c9d3de4a1082
typhonrt <support@typhonjs.io>
2024-05-07 16:47:46 -0700

    comments

--- src/svelte/store/position/index.js
+++ src/svelte/store/position/index.js
@@ -1,2 +1,11 @@
+/**
+ * Provides a reactive compound store and related actions for advanced and optimized positioning of elements including
+ * essential animation / tweening and validation of positional changes. {@link TJSPosition} is the main reactive store
+ * along with the {@link applyPosition} and {@link draggable} actions to respectively attach a `TJSPosition` instance
+ * to an element in a Svelte template and make it draggable.
+ *
+ * @packageDocumentation
+ */
+
 export * from './action';
 export * from './TJSPosition.js';
