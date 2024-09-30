5ec908311d8608ce9989c1775b89463167f11ec1
typhonrt <support@typhonjs.io>
2024-09-30 16:28:37 -0700

    Add colord sub-path export.

--- src/data/color/colord/index.js
+++ src/data/color/colord/index.js
@@ -1,1 +1,10 @@
+/**
+ * Documentation: {@link https://github.com/omgovich/colord#getting-started}
+ *
+ * Note: For the time being `colord` provides "the best" API / support for working with color with Javascript. It
+ * however may not be the long term solution for TRL as it is not maintained.
+ *
+ * @module
+ */
+
 export * from 'colord-typhonjs';
