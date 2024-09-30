c57e14956d767fba8c0f69b33c37e9c198f87aeb
typhonrt <support@typhonjs.io>
2024-09-30 10:33:28 -0700

    Added 118n sub-path export.

---
+++ src/util/i18n/localize.js
@@ -0,0 +1,17 @@
+/**
+ * Localize a string including variable formatting for input arguments. Provide a string ID which defines the localized
+ * template. Variables can be included in the template enclosed in braces and will be substituted using those named
+ * keys.
+ *
+ * Note: This is just a stub implementation and must be implemented for final TRL packaging depending on the platform.
+ *
+ * @param {string}   stringId - The string ID to translate.
+ *
+ * @param {object}   [data] - Provided input data.
+ *
+ * @returns {string} The translated and formatted string
+ */
+export function localize(stringId, data) // eslint-disable-line no-unused-vars
+{
+   return stringId;
+}
