b89cd5229ac230e4d8bb02eec736a70d29e2fc89
typhonrt <support@typhonjs.io>
2025-01-15 16:09:18 -0800

    Add `MinimalWritableFn` and `WritableFn` declarations.

--- _dist/svelte/store/util/index.d.ts
+++ _dist/svelte/store/util/index.d.ts
@@ -1,11 +1,13 @@
 import * as svelte_store from 'svelte/store';
-import { Readable } from 'svelte/store';
+import { Readable, Writable } from 'svelte/store';
 
 /**
  * The minimal requirements of the [writable store contract](https://svelte.dev/docs/svelte-components#script-4-prefix-stores-with-$-to-access-their-values).
  *
  * A minimal writable is a {@link Readable} / subscribable, but only has a `set` method omitting the `update` method
  * from `Writable`.
+ *
+ * @typeParam T - type of data.
  */
 interface MinimalWritable<T> extends Readable<T> {
   /**
@@ -14,6 +16,28 @@ interface MinimalWritable<T> extends Readable<T> {
    */
   set(this: void, value: T): void;
 }
+/**
+ * Combines a minimal writable Svelte store with a callable function. This type allows a minimal writable store to
+ * also act as a function enabling flexible reactive behavior.
+ *
+ * @typeParam T - The type of value stored in the minimal writable store.
+ *
+ * @typeParam Args - The tuple of argument types accepted by the function.
+ *
+ * @typeParam R - The return type of the function.
+ */
+type MinimalWritableFn<T, Args extends unknown[], R = void> = MinimalWritable<T> & ((...args: Args) => R);
+/**
+ * Combines a writable Svelte store with a callable function. This type allows a writable store to also act as a
+ * function enabling flexible reactive behavior.
+ *
+ * @typeParam T - The type of the value stored in the writable store.
+ *
+ * @typeParam Args - The tuple of argument types accepted by the function.
+ *
+ * @typeParam R - The return type of the function.
+ */
+type WritableFn<T, Args extends unknown[], R> = Writable<T> & ((...args: Args) => R);
 
 /**
  * Provides a basic test for a given variable to test if it has the shape of a readable store by having a `subscribe`
@@ -85,6 +109,8 @@ declare function subscribeFirstRest(
 
 export {
   type MinimalWritable,
+  type MinimalWritableFn,
+  type WritableFn,
   isMinimalWritableStore,
   isReadableStore,
   isWritableStore,
--- src/svelte/store/util/types.ts
+++ src/svelte/store/util/types.ts
@@ -1,10 +1,14 @@
-import type { Readable } from 'svelte/store';
+import type {
+   Readable,
+   Writable } from 'svelte/store';
 
 /**
  * The minimal requirements of the [writable store contract](https://svelte.dev/docs/svelte-components#script-4-prefix-stores-with-$-to-access-their-values).
  *
  * A minimal writable is a {@link Readable} / subscribable, but only has a `set` method omitting the `update` method
  * from `Writable`.
+ *
+ * @typeParam T - type of data.
  */
 interface MinimalWritable<T> extends Readable<T> {
   /**
@@ -14,6 +18,32 @@ interface MinimalWritable<T> extends Readable<T> {
   set(this: void, value: T): void;
 }
 
+/**
+ * Combines a minimal writable Svelte store with a callable function. This type allows a minimal writable store to
+ * also act as a function enabling flexible reactive behavior.
+ *
+ * @typeParam T - The type of value stored in the minimal writable store.
+ *
+ * @typeParam Args - The tuple of argument types accepted by the function.
+ *
+ * @typeParam R - The return type of the function.
+ */
+type MinimalWritableFn<T, Args extends unknown[], R = void> = MinimalWritable<T> & ((...args: Args) => R);
+
+/**
+ * Combines a writable Svelte store with a callable function. This type allows a writable store to also act as a
+ * function enabling flexible reactive behavior.
+ *
+ * @typeParam T - The type of the value stored in the writable store.
+ *
+ * @typeParam Args - The tuple of argument types accepted by the function.
+ *
+ * @typeParam R - The return type of the function.
+ */
+type WritableFn<T, Args extends unknown[], R> = Writable<T> & ((...args: Args) => R);
+
 export {
-  MinimalWritable
+  MinimalWritable,
+  MinimalWritableFn,
+  WritableFn
 }
