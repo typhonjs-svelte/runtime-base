a78f006a8e5684f754e4d0b3c85a016aff538b75
typhonrt <support@typhonjs.io>
2025-09-05 01:14:09 -0700

    Added `svelte/action/dom/tooltip` sub-path export.

---
+++ src/svelte/action/dom/tooltip/popoverTooltip.js
@@ -0,0 +1,28 @@
+import { localize }  from '#runtime/util/i18n';
+
+/**
+ * Provides a popover tooltip action that by default uses the elements `title` attribute.
+ *
+ * Depending on the runtime and platform the use of `title` may be replaced with an alternate tooltip implementation.
+ *
+ * @param {HTMLElement} node - Target element.
+ *
+ * @param {string}  tooltip - The tooltip to set.
+ *
+ * @returns {import('svelte/action').ActionReturn<string>} Lifecycle functions.
+ */
+export function popoverTooltip(node, tooltip)
+{
+   node.title = typeof tooltip === 'string' ? localize(tooltip) : null;
+
+   return {
+      /**
+       * @param {string}  newTooltip - Update tooltip.
+       */
+      update: (newTooltip) =>
+      {
+         tooltip = newTooltip;
+         node.title = typeof tooltip === 'string' ? localize(tooltip) : null;
+      }
+   };
+}
