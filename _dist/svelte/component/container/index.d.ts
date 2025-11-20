ba229ef5abef6b38d064f2bdb737f870bf76cfba
typhonrt <support@typhonjs.io>
2025-11-20 04:27:49 -0800

    Add `TJSScrollContainer`.

---
+++ _dist/svelte/component/container/index.d.ts
@@ -0,0 +1,108 @@
+import * as _typhonjs_svelte_runtime_base_svelte_util from '@typhonjs-svelte/runtime-base/svelte/util';
+import * as _typhonjs_svelte_runtime_base_svelte_store_util from '@typhonjs-svelte/runtime-base/svelte/store/util';
+import * as _typhonjs_svelte_runtime_base_svelte_action_dom_style from '@typhonjs-svelte/runtime-base/svelte/action/dom/style';
+import { SvelteComponent } from 'svelte';
+
+/**
+ * Provides a convenient scrollable container / DIV that always allows keyboard scroll navigation by stopping
+ * propagation of page up / down key events when the active element is or is contained by the container.
+ *
+ * Auto serialization of scroll state is handled by providing a store / `scrollTop`.
+ *
+ * A main slot is provided for a content component, but a fallback allows a child content component to be defined
+ * by the `class` and `props` fields in {@link TJSScrollContainerData}.
+ *
+ */
+declare class TJSScrollContainer extends SvelteComponent<
+  TJSScrollContainer.Props,
+  TJSScrollContainer.Events,
+  TJSScrollContainer.Slots
+> {}
+
+/** Event / Prop / Slot type aliases for {@link TJSScrollContainer | associated component}. */
+declare namespace TJSScrollContainer {
+  /** Props type alias for {@link TJSScrollContainer | associated component}. */
+  export type Props = {
+    /** @type {import('.').TJSScrollContainerData} */
+    container?: TJSScrollContainerData;
+    /** @type {import('@typhonjs-svelte/runtime-base/svelte/store/util').MinimalWritable<number>} */
+    scrollLeft?: _typhonjs_svelte_runtime_base_svelte_store_util.MinimalWritable<number>;
+    /** @type {import('@typhonjs-svelte/runtime-base/svelte/store/util').MinimalWritable<number>} */
+    scrollTop?: _typhonjs_svelte_runtime_base_svelte_store_util.MinimalWritable<number>;
+    /** @type {boolean} */
+    allowTabFocus?: boolean;
+    /** @type {boolean} */
+    keyPropagate?: boolean;
+    /** @type {boolean} */
+    gutterStable?: boolean;
+    /** @type {(data: { event: KeyboardEvent | PointerEvent }) => void} */
+    onContextMenu?: (data: { event: KeyboardEvent | PointerEvent }) => void;
+    /**
+     * When true, the inline styles for padding of the parent element to the scroll container element
+     * is adjusted for any visual edge insets / border image applied to the parent element allowing the scroll
+     * container to take up the entire visual content space.
+     *
+     * @type {import('@typhonjs-svelte/runtime-base/svelte/action/dom/style').PadToVisualEdgeSides}
+     */
+    padToVisualEdge?: _typhonjs_svelte_runtime_base_svelte_action_dom_style.PadToVisualEdgeSides;
+    /** @type {{ [key: string]: string | null }} */
+    styles?: { [key: string]: string | null };
+  };
+  /** Events type alias for {@link TJSScrollContainer | associated component}. */
+  export type Events = { [evt: string]: CustomEvent<any> };
+  /** Slots type alias for {@link TJSScrollContainer | associated component}. */
+  export type Slots = { default: {} };
+}
+
+type TJSScrollContainerData = {
+  /**
+   * When true, the scroll container is keyboard navigation focusable.
+   */
+  allowTabFocus?: boolean;
+  /**
+   * By default, the scroll container stops propagation of all keys that are
+   * related to keyboard scrolling accessibility. When true, the scroll container will not capture scrolling key events.
+   */
+  keyPropagate?: boolean;
+  /**
+   * When true, the scrollbar gutter is set to `stable`. This is a convenience
+   * for automatic configuration without a wrapping element to set a CSS variable.
+   */
+  gutterStable?: boolean;
+  /**
+   * Callback to handle context menu
+   * presses.
+   */
+  onContextMenu?: (data: { event: KeyboardEvent | PointerEvent }) => void;
+  /**
+   * When true, the
+   * inline styles for padding of the parent element to the scroll container element is adjusted for any visual edge
+   * insets / border image applied to the parent element allowing the scroll container to take up the entire visual
+   * content space.
+   */
+  padToVisualEdge?: _typhonjs_svelte_runtime_base_svelte_action_dom_style.PadToVisualEdgeSides;
+  /**
+   * A Svelte store that serializes
+   * the scroll left position of the scrollable container.
+   */
+  scrollLeft?: _typhonjs_svelte_runtime_base_svelte_store_util.MinimalWritable<number>;
+  /**
+   * A Svelte store that serializes
+   * the scroll top position of the scrollable container.
+   */
+  scrollTop?: _typhonjs_svelte_runtime_base_svelte_store_util.MinimalWritable<number>;
+  /**
+   * Inline styles to assign to the container.
+   */
+  styles?: {
+    [key: string]: string | null;
+  };
+  /**
+   * A svelte component configuration object
+   * used as the content component when there is no slotted component defined.
+   */
+  svelte?: _typhonjs_svelte_runtime_base_svelte_util.TJSSvelte.Config.Embed;
+};
+
+export { TJSScrollContainer };
+export type { TJSScrollContainerData };
