8e49673b36134e9ddf81ac19934831604510155e
typhonrt <support@typhonjs.io>
2026-01-06 21:47:15 -0800

    Add ES2024 Set methods.

--- src/svelte/reactivity/ReadonlySvelteSet.ts
+++ src/svelte/reactivity/ReadonlySvelteSet.ts
@@ -45,7 +45,7 @@ export class ReadonlySvelteSet<T> implements ReadonlySet<T>, Readable<ReadonlySv
    /**
     * Iterates over values in the set.
     */
-   [Symbol.iterator](): IterableIterator<T>
+   [Symbol.iterator](): SetIterator<T>
    {
       return this.#svelteSet.values();
    }
@@ -61,7 +61,7 @@ export class ReadonlySvelteSet<T> implements ReadonlySet<T>, Readable<ReadonlySv
    /**
     * Returns an iterable of [v,v] pairs for every value `v` in the set.
     */
-   entries(): IterableIterator<[T, T]>
+   entries(): SetIterator<[T, T]>
    {
       return this.#svelteSet.entries();
    }
@@ -73,7 +73,7 @@ export class ReadonlySvelteSet<T> implements ReadonlySet<T>, Readable<ReadonlySv
     *
     * @param thisArg - Optional this reference for callback function.
     */
-   forEach(callbackfn: (value: T, value2: T, set: ReadonlySvelteSet<T>) => void, thisArg?: unknown): void
+   forEach(callbackfn: (value: T, value2: T, set: ReadonlySet<T>) => void, thisArg?: any): void
    {
       for (const v of this.#svelteSet.values())
       {
@@ -94,7 +94,7 @@ export class ReadonlySvelteSet<T> implements ReadonlySet<T>, Readable<ReadonlySv
    /**
     * Despite its name, returns an iterable of the values in the set.
     */
-   keys(): IterableIterator<T>
+   keys(): SetIterator<T>
    {
       return this.#svelteSet.keys();
    }
@@ -102,9 +102,67 @@ export class ReadonlySvelteSet<T> implements ReadonlySet<T>, Readable<ReadonlySv
    /**
     * Returns an iterable of values in the set.
     */
-   values(): IterableIterator<T>
+   values(): SetIterator<T>
    {
       return this.#svelteSet.values();
+   }
+
+   // ES2024 Implementation ------------------------------------------------------------------------------------------
+
+   /**
+    * @returns a new Set containing all the elements in this Set and also all the elements in the argument.
+    */
+   union<U>(other: ReadonlySetLike<U>): Set<T | U>
+   {
+      return this.#svelteSet.union(other);
+   }
+
+   /**
+    * @returns a new Set containing all the elements which are both in this Set and in the argument.
+    */
+   intersection<U>(other: ReadonlySetLike<U>): Set<T & U>
+   {
+      return this.#svelteSet.intersection(other);
+   }
+
+   /**
+    * @returns a new Set containing all the elements in this Set which are not also in the argument.
+    */
+   difference<U>(other: ReadonlySetLike<U>): Set<T>
+   {
+      return this.#svelteSet.difference(other);
+   }
+
+   /**
+    * @returns a new Set containing all the elements which are in either this Set or in the argument, but not in both.
+    */
+   symmetricDifference<U>(other: ReadonlySetLike<U>): Set<T | U>
+   {
+      return this.#svelteSet.symmetricDifference(other);
+   }
+
+   /**
+    * @returns a boolean indicating whether all the elements in this Set are also in the argument.
+    */
+   isSubsetOf(other: ReadonlySetLike<unknown>): boolean
+   {
+      return this.#svelteSet.isSubsetOf(other);
+   }
+
+   /**
+    * @returns a boolean indicating whether all the elements in the argument are also in this Set.
+    */
+   isSupersetOf(other: ReadonlySetLike<unknown>): boolean
+   {
+      return this.#svelteSet.isSupersetOf(other);
+   }
+
+   /**
+    * @returns a boolean indicating whether this Set has no elements in common with the argument.
+    */
+   isDisjointFrom(other: ReadonlySetLike<unknown>): boolean
+   {
+      return this.#svelteSet.isDisjointFrom(other);
    }
 
    // Store subscriber implementation --------------------------------------------------------------------------------
