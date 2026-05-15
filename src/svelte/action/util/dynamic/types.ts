45d1b0cf977950ec5b8d02f3899a17847f678e0f
typhonrt <support@typhonjs.io>
2026-05-14 21:15:12 -0700

    Allow undefined / not function update to dynamic action. Add option to `warn` on undefined / incorrect dynamic action data.

--- src/svelte/action/util/dynamic/types.ts
+++ src/svelte/action/util/dynamic/types.ts
@@ -2,10 +2,13 @@ import type { Action } from 'svelte/action';
 
 /**
  * Options for {@link dynamicAction}. Provide the hosted action and any associated data that is passed to the action.
- * You may update the action or data dynamically to apply new actions.
+ * You may update the action or data dynamically to apply new actions. You may also opt to enable `warn` which will
+ * post a console warning when the updated action is not a function.
  */
 export type DynamicActionOptions = undefined | {
    action: Action<HTMLElement, unknown>;
 
    data?: unknown
+
+   warn?: boolean;
 }
