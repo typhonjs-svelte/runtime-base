847d951d33fc54d0e97f1d9fab15282e1a35ce93
typhonrt <support@typhonjs.io>
2026-07-12 19:43:56 -0700

    Comments

--- src/svelte/util/index.ts
+++ src/svelte/util/index.ts
@@ -1,2 +1,8 @@
-export * from './PropBindingControl';
+/**
+ * Defines API detection for common configuration objects in TRL for data defined Svelte component loading in addition
+ * to other useful Svelte utilities.
+ *
+ * @packageDocumentation
+ */
+
 export * from './TJSSvelte';
